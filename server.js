71f8bb7edde70817d1878f8ecf06442082fc1ddf
FRASER IRVINE (1801054) <f.irvine@rgu.ac.uk>
2022-01-25 16:13:53 +0000

    added validation

--- server.js
+++ server.js
@@ -36,9 +36,20 @@ adminRouter.get('/users', function(req,res){
     res.send("I show all the users!");
 });
 
+//route middleware to validate :name
+adminRouter.param('name', function(req,res,next,name){
+    //do validation on name here
+    //log something so we know its working
+    console.log('doing name validations on ' + name);
+    //once validation is done save the new item in the req
+    req.name = name;
+    //go to the next thing
+    next();
+});
+
 //route with parameters (http://localhost:PORT/admin/users/:name)
 adminRouter.get('/users/:name', function(req,res){
-    res.send('hello' + req.params.name + '!');
+    res.send('hello ' + req.params.name + '!');
 });
 
 
